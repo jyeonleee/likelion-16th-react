9b55595dd5d088f8378c084a782d3e347012a53c
jyeonleee <juyeonlee0203@gmail.com>
2026-03-06 14:46:06 +0900

    learn. React.Fragment - 그룹 목적의 리액트 요소 - 실제 DOM에 렌더링 안됨

--- learing-react/src/App.tsx
+++ learing-react/src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
   // JSX (JavaScript 확장 구문: 마크업 (구조 설계 in JavaScript 파일))
   // JSX는 문(statement)이다? ❌ 값(expression, value)이다? ⭕️
   return (
-    <div>
+    <>
       <header className="header">
         <h1>
           <dfn>
@@ -76,6 +76,6 @@ export default function App() {
           완주를!"
         </small>
       </footer>
-    </div>
+    </>
   );
 }
