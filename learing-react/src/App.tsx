1b3ac0f18778ff829cf1872885a272a6f4c0dfcb
jyeonleee <juyeonlee0203@gmail.com>
2026-03-06 13:24:27 +0900

    learn. JSX는 문이 아니라, 식이다. - React.createElement API로 변환 (TSC에 의해서)

--- learing-react/src/App.tsx
+++ learing-react/src/App.tsx
@@ -1,36 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import * as React from 'react'
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
+/**
+ * JSX (JavaScript eXtension: 자바스크립트 확장 (비표준: 브라우저 해석 못함: SyntaxError))
+ * 빌드(컴파일 + 번들링) 도구에서만 JSX 사용 가능
+ */
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test{" "}
-          <abbr title="Hot Module Replacement">HMR</abbr>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+export default function App() {
+  // return null /* 아무 것도 반환하지 않음 (화면에 아무 것도 그리지 않음) */
+
+  // 함수가 JSX(React.ReactNode 타입) 반환
+  // Build Tools (Vite, Webpack, Turbopack, ....)
+  // *.tsx (TypeScript + JSX) -> TSC -> *.js (React API: React.createElement(type, props, ...children))
+
+  // JSX (JavaScript 확장 구문: 마크업 (구조 설계 in JavaScript 파일))
+  // JSX는 문(statement)이다? 값(expression, value)이다?
+  // return <header className="header">
+  //   <h1>
+  //     <dfn><abbr title="JavaScript eXtension">JSX</abbr></dfn> 기초 배우기
+  //   </h1>
+  // </header>
+
+  // JSX가 아닌, React API(함수)
+  return React.createElement(
+    'header',
+    { className: 'header' },
+    React.createElement(
+      'h1',
+      null,
+      React.createElement(
+        'dfn',
+        null,
+        React.createElement('abbr', { title: 'JavaScript eXtension' }, 'JSX'),
+      ),
+      ' \uAE30\uCD08 \uBC30\uC6B0\uAE30',
+    ),
+  )
 }
-
-export default App;
