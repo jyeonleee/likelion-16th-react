9d558e5e2ed944aace52d9e63a52f967a7cf7101
jyeonleee <juyeonlee0203@gmail.com>
2026-03-12 13:25:08 +0900

    learn. 데이터 + 타입 + 리스트 렌더링 - StaffList 컴포넌트 - data, type 활용 (분리)

--- learning-react/04-리스트-렌더링/src/app/index.tsx
+++ learning-react/04-리스트-렌더링/src/app/index.tsx
@@ -1,9 +1,11 @@
+
+import { StaffList } from '@/components'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      {/*  */}      
+      <StaffList />
     </div>
   )
 }
