6607ec32cb1368b8e5ae6a7f68f07688619653c7
jyeonleee <juyeonlee0203@gmail.com>
2026-03-17 13:48:28 +0900

    practice. 사용자 ID로 할 일 검색 Part 4. - 사용자 입력 디바운스(Debounce) - 응답받지 않은 이전 요청의 경우 중단(Abort) - 에러 핸들링 (try...catch)

--- learning-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
+++ learning-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
@@ -1,52 +1,64 @@
 import { useEffect, useState } from 'react'
+import { getRandomCompletedTodos, getTodos, type Todo } from '@/api/getTodos'
 import S from './TodoSearch.module.css'
 
-// API 엔드포인트(Endpoint)
-const { VITE_API_URL: API_URL } = import.meta.env
-console.log(`${API_URL}/api/todos?userId=${1}`)
-
-// 응답 데이터 타입 지정
-interface ResponseTodosData {
-  message: string
-  todos: Todo[]
-}
-
-interface Todo {
-  id: number
-  content: string
-  completed: boolean
-  userId: number
-}
+// 디바운스 시간(ms)
+const DEBOUNCE_TIME = 500 // 500ms (200~300ms)
 
 export default function TodoSearch() {
-  // 리액트로 하여금 화면을 변경
-  // 선언적 API로 제어 (상태 선언)
-  // - 로딩(loading) 상태
-  const [loading] = useState(false);
-  // - 할 일 목록(todos) 상태
-  const [todos, setTodos] = useState<Todo[]>([]);
-  // - 사용자 ID(userId) 상태
-  const [userId, setUserId] = useState("");
+  const [loading, setLoading] = useState(false)
+  const [todos, setTodos] = useState<Todo[]>([])
+  const [userId, setUserId] = useState('')
 
+  // TODO 1: 상태 업데이트 디바운싱(Debouncing)
+  // 사용자 입력이 멈춘 후 특정 시간이 지난 다음 상태 업데이트 요청 처리 (잦은 요청 완화: 성능 최적화)
+  
+  // TODO 2: 네트워크 요청 중단(abort) ✅
+  // 이전 요청이 아직 응답되지 않은 경우, 이전 요청을 중단해 불필요한 요청이 발생하지 않도록 조치 (성능 최적화)
+
+  // TODO 3: 에러 핸들링 ✅
+  // 에러가 발생한 상황을 사용자에게 알림 (사용자 경험 최적화)
   useEffect(() => {
-    // console.log(`${API_URL}/api/todos?userId=${userId}`);
-    // 비동기 gkatn
 
+    // 아직 응답되지 않은 이전의 네트워크 요청 중단
+    const controller = new AbortController()
+    const { signal } = controller
     
-    if (userId === '') { 
-      setTodos([])
-      return 
-    }
-    const getTodos = async () => {
-      const response = await fetch(`${API_URL}/api/todos?userId=${userId}`);
-      const josn = await response.json()
-      console.log(josn);
-      
+    // 데이터 가져오기 비동기 함수 
+    const fetchTodos = async () => {
+      console.log('데이터 가져오기 요청 시작!')
 
+      if (userId === '') return setTodos([])
+        
+      setLoading(true)
+        
+      try {
+        const todos = await getTodos(userId, { signal })
+        setTodos(todos)
+      } catch(error) {
+        if (error instanceof Error && error.name !== 'AbortError') {
+          console.error('데이터 로드 실패')
+          setTodos([]) // todos 상태 초기화
+        }
+      } finally {
+        if (!signal.aborted) {
+          setLoading(false)
+        }
+      }
     }
-    getTodos()
+
+    // 디바운스: DEBOUNCE_TIME 만큼 사용자 입력이 더 없다면 그 때 데이터 가져오기 요청
+    const debounceId = setTimeout(fetchTodos, DEBOUNCE_TIME)
+
+    // 클린업(정리) 함수
+    return () => {
+      controller.abort() // 중복 또는 응답되지 않은 이전 요청 중단
+      clearTimeout(debounceId) // setTimeout으로 설정된 콜백 함수 실행 중단
+    }
+
   }, [userId])
 
+  const randomCompletedTodos = getRandomCompletedTodos(todos)
 
   return (
     <section className={S.container}>
@@ -70,29 +82,33 @@ export default function TodoSearch() {
         />
       </div>
 
-      {/* 할 일 목록 템플릿 */}
-      <ul className={S.list}>
-        <li className={S.item}>
-          <span className={S.textContent}>아직 완료 못한 일</span>
-          <span aria-label="예정" style={{ opacity: 0.3 }}>
-            ❎
-          </span>
-        </li>
-        <li className={S.item}>
-          <span className={`${S.textContent} ${S.completed}`}>완료한 일</span>
-          <span aria-label="완료" style={{ opacity: 1 }}>
-            ✅
-          </span>
-        </li>
-      </ul>
+      {!loading && todos.length > 0 && (
+        <ul className={S.list}>
+          {randomCompletedTodos.map(({ id, content, completed }) => (
+            <li key={id} className={S.item}>
+              <span
+                className={`${S.textContent} ${completed ? S.completed : ''}`}
+              >
+                {content}
+              </span>
+              <span
+                aria-label={completed ? '완료' : '예정'}
+                style={{ opacity: completed ? 1 : 0.3 }}
+              >
+                {completed ? '✅' : '❎'}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
 
-      {/* 상태 알림 템플릿 */}
       <div role="status" className={S.statusRegion}>
-        {/* 데이로 로딩 중 표시 메시지 */}
-        <p className={S.loading}>데이터를 가져오고 있습니다...</p>
-        {/* 검색 결과가 없을 경우 표시 메시지 */}
-        <p>검색 결과가 없습니다.</p>
+        {loading && <p className={S.loading}>데이터를 가져오고 있습니다...</p>}
+
+        {!loading && userId && todos.length === 0 && (
+          <p>검색 결과가 없습니다.</p>
+        )}
       </div>
     </section>
-  );
+  )
 }
