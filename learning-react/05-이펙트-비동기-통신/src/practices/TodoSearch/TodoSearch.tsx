ccd9feb27bab28f5c459ab29b6e8174f2cbf5a32
jyeonleee <juyeonlee0203@gmail.com>
2026-03-17 13:01:14 +0900

    practice. 사용자 ID로 할 일 검색 Part 1. - 상태 선언 및 타입 지정 - 데이터 페칭 (API URL 설정) - 조건부 렌더링 (로딩, 데이터 없음, 리스트 렌더링)
    practice. 사용자 ID로 할 일 검색 Part 2.
    - API 데이터 가져오기 함수 분리 (타입 포함)
    - 파생된 상태를 활용해 랜덤 완료 상태 목록 구성 (유틸리티 함수 사용)
    
    practice. 사용자 ID로 할 일 검색 Part 3.
    - getRandomCompletedTodos 유틸리티 함수
    - src/api/getTodos.ts

--- learning-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
+++ learning-react/05-이펙트-비동기-통신/src/practices/TodoSearch/TodoSearch.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from 'react'
 import S from './TodoSearch.module.css'
 
+// API 엔드포인트(Endpoint)
+const { VITE_API_URL: API_URL } = import.meta.env
+console.log(`${API_URL}/api/todos?userId=${1}`)
+
+// 응답 데이터 타입 지정
+interface ResponseTodosData {
+  message: string
+  todos: Todo[]
+}
+
+interface Todo {
+  id: number
+  content: string
+  completed: boolean
+  userId: number
+}
+
 export default function TodoSearch() {
-  
+  // 리액트로 하여금 화면을 변경
+  // 선언적 API로 제어 (상태 선언)
+  // - 로딩(loading) 상태
+  const [loading] = useState(false);
+  // - 할 일 목록(todos) 상태
+  const [todos, setTodos] = useState<Todo[]>([]);
+  // - 사용자 ID(userId) 상태
+  const [userId, setUserId] = useState("");
+
+  useEffect(() => {
+    // console.log(`${API_URL}/api/todos?userId=${userId}`);
+    // 비동기 gkatn
+
+    
+    if (userId === '') { 
+      setTodos([])
+      return 
+    }
+    const getTodos = async () => {
+      const response = await fetch(`${API_URL}/api/todos?userId=${userId}`);
+      const josn = await response.json()
+      console.log(josn);
+      
+
+    }
+    getTodos()
+  }, [userId])
+
+
   return (
     <section className={S.container}>
       <header>
@@ -19,23 +65,21 @@ export default function TodoSearch() {
           min={1}
           max={20}
           placeholder="사용자 ID를 입력하세요 (예: 1 ~ 20)"
+          value={userId}
+          onChange={(e) => setUserId(e.target.value.trim())}
         />
       </div>
 
       {/* 할 일 목록 템플릿 */}
       <ul className={S.list}>
         <li className={S.item}>
-          <span className={S.textContent}>
-            아직 완료 못한 일
-          </span>
+          <span className={S.textContent}>아직 완료 못한 일</span>
           <span aria-label="예정" style={{ opacity: 0.3 }}>
             ❎
           </span>
         </li>
         <li className={S.item}>
-          <span className={`${S.textContent} ${S.completed}`}>
-            완료한 일
-          </span>
+          <span className={`${S.textContent} ${S.completed}`}>완료한 일</span>
           <span aria-label="완료" style={{ opacity: 1 }}>
             ✅
           </span>
@@ -50,5 +94,5 @@ export default function TodoSearch() {
         <p>검색 결과가 없습니다.</p>
       </div>
     </section>
-  )
+  );
 }
