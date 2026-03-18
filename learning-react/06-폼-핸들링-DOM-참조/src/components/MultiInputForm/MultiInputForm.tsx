7cf3b8204afd8345028e4fcd9387b67e25b09485
jyeonleee <juyeonlee0203@gmail.com>
2026-03-18 15:17:56 +0900

    learn. key 속성을 사용한 리액트 폼 초기화(Reset) - 폼 상태 초기화를 손쉽게 하는 초기 상태 값 컴포넌트 외부에 선언 - 리액트 컴포넌트는 key 값이 변경되면 상태가 초기화 됨 - 참고: https://ko.react.dev/learn/preserving-and-resetting-state#resetting-a-form-with-a-key

--- learning-react/06-폼-핸들링-DOM-참조/src/components/MultiInputForm/MultiInputForm.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/components/MultiInputForm/MultiInputForm.tsx
@@ -1,41 +1,45 @@
-import { useId } from "react";
-import NicknameField from "./parts/NicknameField";
-import EmailField from "./parts/EmailField";
-import PasswordField from "./parts/PasswordField";
-import PasswordConfirmField from "./parts/PasswordConfirmField";
-import S from "./MultiInputForm.module.css";
+import { useId, useState } from 'react'
+import NicknameField from './parts/NicknameField'
+import EmailField from './parts/EmailField'
+import PasswordField from './parts/PasswordField'
+import PasswordConfirmField from './parts/PasswordConfirmField'
+import S from './MultiInputForm.module.css'
 
-// -------------------------------------------------------------------
+// ----------------------------------------------------------------------
 // 실습 가이드
-// -------------------------------------------------------------------
-// 1. 모든 필드의 초기값을 가진 객체 'INITIAL_STATE'를 컴포넌트 외부에 정의하세요.
-// 2. INITIAL_STATE를 사용하는 하나의 'formState' 상태(객체형)를 선언합니다.
+// ----------------------------------------------------------------------
+// 1. 모든 필드의 초기값을 가진 객체 'INITIAL_FORM_STATE'를 컴포넌트 외부에 정의하세요.
+// 2. INITIAL_FORM_STATE를 사용하는 하나의 'formState' 상태(객체형)를 선언합니다.
 // 3. name과 value를 인자로 받아 formState를 업데이트하는 함수를 작성하세요.
 // 4. 폼 초기화(reset) 이벤트 핸들러를 작성하세요.
-// -------------------------------------------------------------------
+// ----------------------------------------------------------------------
 
-interface FormState {
-  nickname: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
+const INITIAL_FORM_STATE = {
+  nickname: '',
+  email: '',
+  password: '',
+  passwordConfirm: '',
 }
 
-const INITIAL_FORM_STATE: FormState = {
-  nickname: "",
-  email: "",
-  password: "",
-  passwordConfirm: "",
-};
+// 사용자 정의 타입 알리아스 (custom type alias)
+type FormState = typeof INITIAL_FORM_STATE
+type FormStateKey = keyof FormState
 
 export default function MultiInputForm() {
-  const sectionId = useId();
-  const [formState] = useState(INITIAL_FORM_STATE);
-  
-  const nickname = "";
-  const email = "";
-  const password = "";
-  const passwordConfirm = "";
+  const sectionId = useId()
+  const [formState, setFormState] = useState<FormState>(INITIAL_FORM_STATE)
+  const [formResetKey, setFormResetKey] = useState(0)
+
+  const changeFormState = (name: FormStateKey, value: string) => {
+    setFormState({ ...formState, [name]: value })
+  }
+
+  const handleReset = () => {
+    console.log('폼 상태 초기화')
+    // 리액트가 제어하는 폼 상태 초기화 하기
+    setFormState(INITIAL_FORM_STATE)
+    setFormResetKey((prev) => prev + 1)
+  }
 
   return (
     <article className={S.card} aria-labelledby={sectionId}>
@@ -48,14 +52,23 @@ export default function MultiInputForm() {
         </p>
       </header>
 
-      <form className={S.form}>
-        <NicknameField value={nickname} onChange={() => {}} />
-        <EmailField value={email} onChange={() => {}} />
-        <PasswordField value={password} onChange={() => {}} />
+      <form key={formResetKey} className={S.form} onReset={handleReset}>
+        <NicknameField
+          value={formState.nickname}
+          onChange={(value) => changeFormState('nickname', value)}
+        />
+        <EmailField
+          value={formState.email}
+          onChange={(value) => changeFormState('email', value)}
+        />
+        <PasswordField
+          value={formState.password}
+          onChange={(value) => changeFormState('password', value)}
+        />
         <PasswordConfirmField
-          value={passwordConfirm}
-          basePassword={password}
-          onChange={() => {}}
+          value={formState.passwordConfirm}
+          basePassword={formState.password}
+          onChange={(value) => changeFormState('passwordConfirm', value)}
         />
         <div role="group" className={S.buttonGroup}>
           <button type="reset" className={S.resetButton}>
@@ -67,5 +80,5 @@ export default function MultiInputForm() {
         </div>
       </form>
     </article>
-  );
+  )
 }
