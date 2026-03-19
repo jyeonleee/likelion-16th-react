a84f7f7ec8deeb647dfe2640b51f7a06983f1b1c
jyeonleee <juyeonlee0203@gmail.com>
2026-03-19 09:02:59 +0900

    ready. 수업파일준비

--- learning-react/06-폼-핸들링-DOM-참조/src/app/index.tsx
+++ learning-react/06-폼-핸들링-DOM-참조/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { MultiInputForm } from '@/components'
+import { RefStudy } from '@/components'
 import S from './style.module.css'
 
 export default function App() {
   return (
     <div className={S.container}>
-      <MultiInputForm />
+      <RefStudy />
     </div>
   )
 }
